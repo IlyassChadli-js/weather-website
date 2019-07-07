23dc640f383f3fdca509ab3941f163aaf63af14f
Ilyass Chadli <ilyasschadli.dev.com>
2019-07-08 00:01:14 +0100

    Remove unncessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#message-1')
